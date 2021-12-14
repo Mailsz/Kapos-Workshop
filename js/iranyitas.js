c18e1c24e5dadb9daa106bc27fee8ac6235fbb67
Akos31 <akoskelemen2005@gmail.com>
2021-12-14 11:34:47 +0100

    comtitt
    comit

---
+++ js/iranyitas.js
@@ -0,0 +1,23 @@
+$("button[name=egyjatekos]").click(function() {
+  setTimeout(function() {
+    window.location.href = "singleplayer.html"
+}, 100);
+})
+
+$("button[name=bejelentkezes]").click(function() {
+  setTimeout(function() {
+    window.location.href = "login.html"
+}, 100);
+})
+
+$("button[name=regisztralas]").click(function() {
+  setTimeout(function() {
+    window.location.href = "regisztralas.html"
+}, 100);
+})
+
+$("button[name=beallitasok]").click(function() {
+  setTimeout(function() {
+    window.location.href = "beallitasok.html"
+}, 100);
+})
