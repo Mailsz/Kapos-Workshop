8a88a840e0abe8b49061dd6ba2a7438b0ac2a7a8
David <game.net0218@gmail.com>
2021-12-21 22:17:32 +0100

    szolista meg mittudomen faradt vagyok
    komolyan genyo 10 ora van azt se tudom mit csinalok

--- nehezseg.py
+++ nehezseg.py
@@ -1,44 +1,45 @@
 import string
-#import MySQLdb
 
-file=open("Angol.txt", "r", encoding="utf-8")
-#szavak beolvasása
-szavak=[]
+# import MySQLdb
+
+file = open("Angol.txt", "r", encoding="utf-8")
+# szavak beolvasása
+szavak = []
 for i in file:
     szavak.append(i.strip("\n").lower())
-abc=string.ascii_lowercase
+abc = string.ascii_lowercase
 
-#szavak betűkké változtatása
-szavak2=[]
+# szavak betűkké változtatása
+szavak2 = []
 
 for szo in szavak:
-    temp=""
+    temp = ""
     for i in range(len(abc)):
-        if(abc[i] in szo):
-            temp+=abc[i]
+        if (abc[i] in szo):
+            temp += abc[i]
     szavak2.append(temp)
 file.close()
 
-#szavak értékekké változtatása betűik gyakoriság alapján
-gyakor=open("betugyakorisag", "r", encoding="utf-8")
-ertekek=[]
-gyakorisag=[]
-#gyakorisag beolvasása
+# szavak értékekké változtatása betűik gyakoriság alapján
+gyakor = open("betugyakorisag", "r", encoding="utf-8")
+ertekek = []
+gyakorisag = []
+# gyakorisag beolvasása
 for i in gyakor:
     gyakorisag.append(float(i.strip("\n")))
-#lista feltolese 0-kal
+# lista feltolese 0-kal
 for i in range(len(szavak)):
     ertekek.append(0)
 
-#szavak ertekenek kiszamolasa
-i=0
+# szavak ertekenek kiszamolasa
+i = 0
 for string in szavak2:
     for j in range(len(string)):
-        ertekek[i] +=  0.125 - gyakorisag[abc.find(string[j])]
-    ertekek[i]=round(ertekek[i], 3)
-    i+=1
+        ertekek[i] += 0.125 - gyakorisag[abc.find(string[j])]
+    ertekek[i] = round(ertekek[i], 3)
+    i += 1
 
-#besorolas
+# besorolas
 
 mydb = MySQLdb.connect(
     host="localhost",
@@ -47,7 +48,7 @@ mydb = MySQLdb.connect(
     database="dusza"
 )
 
-j=0
+j = 0
 for szam in ertekek:
     mycursor = mydb.cursor()
     if (szam < 0.3):
