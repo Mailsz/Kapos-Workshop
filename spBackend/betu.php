54180c43443fb9d8410eefff6d166b2850130c85
David <game.net0218@gmail.com>
2022-01-07 22:50:45 +0100

    kicsi valtoztatasok

--- spBackend/betu.php
+++ spBackend/betu.php
@@ -1,26 +1,25 @@
 <?php
-  session_start();
-  if (isset($_POST['betu'])) {
+session_start();
+if (isset($_POST['betu'])) {
     $betu = $_POST['betu'];
     if (!in_array($betu, $_SESSION['spTippeltBetuk'])) {
-      if (str_contains($_SESSION['spSzo'],$betu)) {
-        /*Ha jó a betű*/
-        $betuk = str_split($_SESSION['spSzo']);
-        $kitalalt = str_split($_SESSION['spKitalaltBetuk']);
-        for ($i=0; $i < strlen($_SESSION['spSzo']); $i++) {
-          if ($betuk[$i]==$betu) {
-            $kitalalt[$i] = $betu;
-          }
+        if (str_contains($_SESSION['spSzo'], $betu)) {
+            /*Ha jó a betű*/
+            $betuk = str_split($_SESSION['spSzo']);
+            $kitalalt = str_split($_SESSION['spKitalaltBetuk']);
+            for ($i = 0; $i < strlen($_SESSION['spSzo']); $i++) {
+                if ($betuk[$i] == $betu) {
+                    $kitalalt[$i] = $betu;
+                }
+            }
+            $_SESSION['spKitalaltBetuk'] = implode($kitalalt);
+            echo implode($kitalalt);
+        } else {
+            /*Hiba szám hozzáadása*/
+            $_SESSION['spHiba'] += 1;
+            echo $_SESSION['spHiba'];
         }
-        $_SESSION['spKitalaltBetuk'] = implode($kitalalt);
-        echo implode($kitalalt);
-      }
-      else {
-        /*Hiba sám hozzáadása*/
-        $_SESSION['spHiba']+=1;
-        echo $_SESSION['spHiba'];
-      }
-      array_push($_SESSION["spTippeltBetuk"],$betu);
+        array_push($_SESSION["spTippeltBetuk"], $betu);
     }
-  }
- ?>
+}
+?>
