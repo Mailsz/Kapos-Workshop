6b714de22ea295d0bf87dd3884d19a1a1a47730d
Akos31 <akoskelemen2005@gmail.com>
2022-01-19 09:26:05 +0100

    kep
    ele

--- egeszsegestaplalkozas/minden.js
+++ egeszsegestaplalkozas/minden.js
@@ -9,24 +9,24 @@ setTimeout(function() {
   $(".c").hide(1000)
   $(".d").hide(1000)
 
-},5000)
+},10000)
 setTimeout(function() {
   $(".b").show(1000)
   $(".a").hide(1000)
   $(".c").hide(1000)
   $(".d").hide(1000)
 
-},10000)
+},20000)
 setTimeout(function() {
   $(".c").show(1000)
   $(".b").hide(1000)
   $(".a").hide(1000)
   $(".d").hide(1000)
 
-},15000)
+},40000)
 setTimeout(function() {
   $(".d").show(1000)
   $(".b").hide(1000)
   $(".a").hide(1000)
   $(".c").hide(1000)
-},20000)
+},60000)
