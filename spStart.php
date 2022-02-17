8b3358eab2cb43885d31ade841b3af19fe4088cd
Asdfr123011 <csizmadiadorian2003@gmail.com>
2022-02-17 13:22:39 +0100

    magyarra áttírás
    áttírtam amiket az akos kert magyarra

--- spStart.php
+++ spStart.php
@@ -10,27 +10,27 @@
 <div class="beallitas">
     <form class="" action="singleplayer.php" method="post">
 
-      <label for="">Difficulty:</label>
+      <label for="">Nehézségi szint:</label>
       <select class="" name="difficulty">
-        <option value="konnyu">easy</option>
-        <option value="kozepes">normal</option>
-        <option value="nehez">hard</option>
+        <option value="konnyu">Könnyű</option>
+        <option value="kozepes">Normál</option>
+        <option value="nehez">Nehéz</option>
       </select>
-      <label for="">Language:</label>
+      <label for="">Nyelv:</label>
       <select class="" name="language">
-        <option value="angol">engish</option>
-        <option value="magyar">hungarian</option>
+        <option value="angol">Engish</option>
+        <option value="magyar">Magyar</option>
       </select>
 
-      <label for="">Time:</label>
+      <label for="">Időkorlát:</label>
       <select class="" name="time">
         <option value="30">30</option>
         <option value="60">60</option>
         <option value="90">90</option>
         <option value="120">120</option>
       </select>
-      <button type="submit" name="button">Start</button>
-      <button type="submit" name="button" id="start">Start</button>
+      <!--<button type="submit" name="button">Indítás</button>-->
+      <button type="submit" name="button" id="start">Indítás</button>
 
     </form>
 </div>
