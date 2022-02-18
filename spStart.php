b4a1cf92247bb94087a845edad46fba58fd79a12
kpeti10 <kisspeti10010@gmail.com>
2022-02-18 11:30:24 +0100

    spStart formázás
    Singleplayer start ablak elfogadhaóvá tétele

--- spStart.php
+++ spStart.php
@@ -10,25 +10,36 @@
 <div class="beallitas">
     <form class="" action="singleplayer.php" method="post">
 
-      <label for="">Nehézségi szint:</label>
+      <label class="opcio" for="">Nehézségi szint:</label>
+      <br>
       <select class="" name="difficulty">
         <option value="konnyu">Könnyű</option>
         <option value="kozepes">Normál</option>
         <option value="nehez">Nehéz</option>
       </select>
-      <label for="">Nyelv:</label>
+      <br>
+      <br>
+      <br>
+      <label class="opcio" for="">Nyelv:</label>
+      <br>
       <select class="" name="language">
         <option value="angol">Engish</option>
         <option value="magyar">Magyar</option>
       </select>
-
-      <label for="">Időkorlát:</label>
+      <br>
+      <br>
+      <br>
+      <label for="" class="opcio" >Időkorlát:</label>
+      <br>
       <select class="" name="time">
         <option value="30">30</option>
         <option value="60">60</option>
         <option value="90">90</option>
         <option value="120">120</option>
       </select>
+      <br>
+      <br>
+      <br>
       <!--<button type="submit" name="button">Indítás</button>-->
       <button type="submit" name="button" id="start">Indítás</button>
 
